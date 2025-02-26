9576269cabbd895cb6b1c9c1efc8304256815dc4
Priyanshu Priyam <172436044+ppriyanshu26@users.noreply.github.com>
2025-02-26 23:48:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,12 @@ def load_resources():
 
 def predict_clickbait(text):
     load_resources()  # Ensure model and tokenizer are loaded
-    return text
+    test_input = [text]
+    test_sequence = tokenizer.texts_to_sequences(test_input)
+    test_padded = tf.keras.preprocessing.sequence.pad_sequences(test_sequence, maxlen=100)
+    output = model.predict(test_padded)
+    prediction = "Possible Clickbait" if output[0][0] < 0.9327575 else "Not Clickbait"
+    return prediction
 
 @app.route("/predict", methods=["POST"])
 def predict():
